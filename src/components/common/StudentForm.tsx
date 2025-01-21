ea573c2d6ed363d331acb2b0ef6d35ce52fe0532
istebsharul <mdistebsharulbari@gmail.com>
2025-01-22 00:00:40 +0530

    Issue fixed related to type any

--- src/components/common/StudentForm.tsx
+++ src/components/common/StudentForm.tsx
@@ -1,29 +1,34 @@
-"use client"
+"use client";
 import { useState, useEffect, useRef } from "react";
 import toast from "react-hot-toast";
 import axios from "axios";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
 const scriptURL = process.env.SCRIPT_URL;
 
-const StudentFormModal = ({ isOpen, closeModal }: { isOpen: boolean; closeModal: () => void }) => {
-  const [name, setName] = useState("");
-  const [selectedClass, setSelectedClass] = useState("Montessori1");
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [email, setEmail] = useState("");
-  
+const StudentFormModal = ({
+  isOpen,
+  closeModal,
+}: {
+  isOpen: boolean;
+  closeModal: () => void;
+}) => {
+  const [name, setName] = useState<string>("");
+  const [selectedClass, setSelectedClass] = useState<string>("Montessori1");
+  const [phoneNumber, setPhoneNumber] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+
   const modalRef = useRef<HTMLDivElement>(null); // Ref for modal container
 
   // Close modal if clicked outside
   useEffect(() => {
-    console.log(scriptURL);
     const handleClickOutside = (event: MouseEvent) => {
       if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
         closeModal();
       }
     };
-    
+
     if (isOpen) {
       document.addEventListener("mousedown", handleClickOutside);
     }
@@ -33,33 +38,36 @@ const StudentFormModal = ({ isOpen, closeModal }: { isOpen: boolean; closeModal:
     };
   }, [isOpen, closeModal]);
 
-  const handleSubmit = async (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-  
+
     const formData = new FormData();
-    formData.append('name', name);
-    formData.append('class', selectedClass);
-    formData.append('phone', phoneNumber);
-    formData.append('email', email);
-  
+    formData.append("name", name);
+    formData.append("class", selectedClass);
+    formData.append("phone", phoneNumber);
+    formData.append("email", email);
+
     if (!scriptURL) {
       console.error("SCRIPT_URL is not defined");
       toast.error("Submission failed: Missing script URL.");
       return;
     }
-  
+
     // Using toast.promise for better user feedback
     toast.promise(
-      axios.post(scriptURL, formData), // Axios request
+      axios.post(scriptURL, formData),
       {
-        loading: 'Processing...',
+        loading: "Processing...",
         success: () => {
-          closeModal(); // Close modal after successful submission
-          return 'Thank you! We will contact you shortly.';
+          closeModal();
+          return "Thank you! We will contact you shortly.";
+          setName('');
+          setPhoneNumber('');
+          setEmail('')
         },
-        error: (err:any) => {
+        error: (err: unknown) => {
           console.error("Error submitting form:", err);
-          return 'Could not save. Please try again later.';
+          return "Could not save. Please try again later.";
         },
       }
     );
@@ -73,10 +81,14 @@ const StudentFormModal = ({ isOpen, closeModal }: { isOpen: boolean; closeModal:
         ref={modalRef}
         className="relative bg-blue-50 p-8 rounded-xl shadow-lg w-96"
       >
-        <h2 className="text-2xl font-bold mb-4 text-center text-red-500 mb-5">Student Registration</h2>
+        <h2 className="text-2xl font-bold mb-4 text-center text-red-500 mb-5">
+          Student Registration
+        </h2>
         <form onSubmit={handleSubmit} className="space-y-4 rounded">
           <div>
-            <label htmlFor="name" className="block ">Student Name</label>
+            <label htmlFor="name" className="block">
+              Student Name
+            </label>
             <input
               type="text"
               id="name"
@@ -88,7 +100,9 @@ const StudentFormModal = ({ isOpen, closeModal }: { isOpen: boolean; closeModal:
           </div>
 
           <div>
-            <label htmlFor="class" className="block">Class</label>
+            <label htmlFor="class" className="block">
+              Class
+            </label>
             <div className="relative">
               <select
                 id="class"
@@ -104,7 +118,9 @@ const StudentFormModal = ({ isOpen, closeModal }: { isOpen: boolean; closeModal:
           </div>
 
           <div>
-            <label htmlFor="phoneNumber" className="block">Phone Number (Required)</label>
+            <label htmlFor="phoneNumber" className="block">
+              Phone Number (Required)
+            </label>
             <input
               type="text"
               id="phoneNumber"
@@ -116,7 +132,9 @@ const StudentFormModal = ({ isOpen, closeModal }: { isOpen: boolean; closeModal:
           </div>
 
           <div>
-            <label htmlFor="email" className="block">Email (Optional)</label>
+            <label htmlFor="email" className="block">
+              Email (Optional)
+            </label>
             <input
               type="email"
               id="email"
@@ -127,7 +145,10 @@ const StudentFormModal = ({ isOpen, closeModal }: { isOpen: boolean; closeModal:
           </div>
 
           <div className="flex justify-between items-center">
-            <button type="submit" className="w-full bg-blue-500 hover:bg-green-500 text-white px-4 py-2 rounded-full">
+            <button
+              type="submit"
+              className="w-full bg-blue-500 hover:bg-green-500 text-white px-4 py-2 rounded-full"
+            >
               Submit
             </button>
           </div>
